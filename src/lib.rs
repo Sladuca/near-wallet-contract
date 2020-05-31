bdc7f9b061dfd84bbcc7ebfed9f305532d939d61
Sebastien La Duca <sladuca777@gmail.com>
2020-05-30 20:49:41 -0400

    redesign contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use near_sdk::collections::Map;
 use near_sdk::json_types::{U128};
-use near_sdk::{env, near_bindgen, ext_contract, AccountId, Balance};
+use near_sdk::{env, near_bindgen, AccountId, Promise};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -9,89 +9,102 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 // Prepaid gas for making a single simple call.
 const SINGLE_CALL_GAS: u64 = 1_000_000_000_000_000_000;
 
-
-#[ext_contract(chiron)]
-pub trait ChironCrossContract {
-  fn create_account(&mut self);
-  fn transfer(&mut self, recipient: AccountId, amount: U128);
-  fn get_total_supply(&self) -> U128;
-  fn get_balance(&self, owner_id: AccountId) -> U128;
-}
-
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Account {
   username: String,
   account_id: AccountId,
-  pk: Vec<u8> // public key used to access this account
-}
-
-impl Account {
-  fn new(username: String) -> Self {
-    Account {
-      username: username,
-      account_id: env::signer_account_id(),
-      pk: env::signer_account_pk()
-    }
-  }
-
-  fn authorize(&self) -> bool {
-    return env::signer_account_pk() == self.pk
-  }
 }
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct PeleonContractWallet {
-  mentorship_contract: AccountId,
-  chiron_token_contract: AccountId,
+pub struct ContractWallet {
+  owner_id: AccountId,
+  owner_pk: Vec<u8>,
+  manager_id: AccountId,
+  managet_pk: Vec<u8>,
+  gateway_contract: AccountId,
   accounts: Map<Vec<u8>, Account>
 }
 
-#[near_bindgen]
-impl PeleonContractWallet {
-  pub fn create_account(&mut self, username: String) {
-    let new_account = Account::new(username);
-    let account_hash = Self::get_account_hash(new_account.account_id.to_owned(), &mut new_account.pk.to_owned());
-    assert!(self.accounts.get(&account_hash).is_none(), "account already exists");
-    self.set_account(account_hash, new_account);
-    chiron::create_account(&self.chiron_token_contract, 0, SINGLE_CALL_GAS);
+impl Default for ContractWallet {
+  fn default() -> Self {
+    panic!("Contract wallet must be initialized before use!")
   }
 }
 
-impl PeleonContractWallet {
-  fn get_account_hash(account_id: AccountId, pk: &mut Vec<u8>) -> Vec<u8> {
-    let mut account_bytes = account_id.as_bytes().to_vec();
-    account_bytes.append(pk);
-    env::sha256(account_bytes.as_slice())
-  }
 
-  fn set_account(&mut self, account_hash: Vec<u8>, account: Account) {
-    self.accounts.insert(&account_hash, &account);
-  }
-
-  // panics if account DNE
-  fn get_account(&self, account_hash: Vec<u8>) -> Account {
-    match self.accounts.get(&account_hash) {
-      Some(account) => account,
-      None => panic!("account does not exist")
+#[near_bindgen]
+impl ContractWallet {
+  #[init]
+  pub fn new(manager_id: AccountId, manager_pk: Vec<u8>, gateway_contract: AccountId) -> Self {
+    assert!(env::is_valid_account_id(manager_id.as_bytes()), "Manager's account ID is invalid!");
+    ContractWallet {
+      owner_id: env::signer_account_id(),
+      owner_pk: env::signer_account_pk(),
+      manager_id: manager_id,
+      managet_pk: manager_pk,
+      gateway_contract: gateway_contract.into(),
+      accounts: Map::new("peleon_account".as_bytes().to_vec()),
     }
   }
 
-  fn check_account_authorized(&self) -> bool {
-    let account_hash = Self::get_account_hash(env::signer_account_id(), &mut env::signer_account_pk());
-    let account = self.get_account(account_hash);
-    account.authorize()
+  fn only_owner(&self) {
+    if env::signer_account_pk() != self.owner_pk {
+      panic!("Only contract owner can perform this action!")
+    }
   }
 
-  // ----- CHIRON CALLS ----- //
-  
-  fn transfer_chiron(&self, recipient: AccountId, amount: U128) {
-    assert!(self.check_account_authorized(), "incorrect wallet");
-    chiron::transfer(recipient, amount, &self.chiron_token_contract, 0, SINGLE_CALL_GAS);
+  fn only_manager(&self) {
+    if env::signer_account_pk() != self.managet_pk && env::signer_account_pk() != self.owner_pk {
+      panic!("Only contract manager or owner can perform this action!")
+    }
   }
 
-  fn get_total_chiron_supply(&self, owner_id: AccountId) -> U128 {
-    chiron::get_total_supply()
+  pub fn transfer_ownership(&mut self, new_owner_id: AccountId, new_owner_pk: Vec<u8>) {
+    self.only_owner();
+    assert!(env::is_valid_account_id(new_owner_id.as_bytes()), "New owner's account ID is invalid!");
+    self.owner_id = new_owner_id;
+    self.owner_pk = new_owner_pk;
   }
-  fn get_chiron_balance(&self, owner_id: AccountId) -> U128 {}
+
+  pub fn update_manager(&mut self, new_manager_id: AccountId, new_manager_pk: Vec<u8>) {
+    self.only_owner();
+    assert!(env::is_valid_account_id(new_manager_id.as_bytes()), "New manager's account ID is invalid!");
+    self.manager_id = new_manager_id;
+    self.managet_pk = new_manager_pk;
+  }
+
+  fn create_account(&self, username: String) -> AccountId {
+    self.only_manager();
+    let mut account_id = username.clone();
+    account_id.push_str(&env::current_account_id());
+    let account_hash = Self::get_account_hash(&username, &account_id);
+    // panic if account id already taken
+    match self.get_account(account_hash) {
+      Some(_) => panic!("Account with given username already exists!"),
+      None => {
+        let new_account = Account {
+          username: username,
+          account_id: account_id.clone()
+        };
+        // create the account
+        Promise::new(account_id.clone())
+          .create_account();
+        account_id
+      }
+    }
+  }
+
+  fn get_account_hash(username: &String, account_id: &String) -> Vec<u8> {
+    let account_bytes = [username.as_bytes(), account_id.as_bytes()].concat();
+    env::keccak256(&account_bytes)
+  }
+
+  fn get_account(&self, account_hash: Vec<u8>) -> Option<Account> {
+    self.accounts.get(&account_hash)
+  }
+
+  fn set_account(&mut self, account_hash: &Vec<u8>, account: &Account) -> Option<Account> {
+    self.accounts.insert(account_hash, account)
+  }
 }
